fed9581774715ac61d8833425f4d32a5a1b7f44b
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-10-18 21:43:27 +0800

    跳轉

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('r2', function () {
     return view('welcome');
 });
 
+Route::get('r1',function(){
+    return redirect('r2');
+});
